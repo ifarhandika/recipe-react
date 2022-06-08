cbd0fabc2112385bc1f855403b5e8faa902ee914
ifarhandika <ifarhandika@gmail.com>
2022-06-08 11:15:15 +0700

    cleaning up

--- src/pages/RecipeDetails.jsx
+++ src/pages/RecipeDetails.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
-import details from "../components/details"
 
 const RecipeDetails = () => {
   const [recipeDetails, setRecipeDetails] = useState([])
